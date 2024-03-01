d90aea00ed8620c8fd03bfb1606c2d93e2d017f9
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-03-01 10:05:53 +0100

    hecho_en_clase

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,14 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/path') //the endpoint path
+    .get( //the http verb that we want to be available at the previous path
+      EntityController.index) // the function that will attend requests for that http verb and that path
+    .post( //we can chain more http verbs for the same endpoint
+      EntityController.create) // the function that will attend requests for that http verb and that path
+    .put( 
+      EntityController.update)
+    .delete( 
+      EntityController.destroy)
 }
 export default loadFileRoutes
